dd471cc06628da6a85877edfe7696ecd5ac98182
Sampa Nayak <sampanayak987@gmail.com>
2025-08-25 00:29:14 +0530

    Add files via upload
    print a character is vowel or not.

---
+++ vowel.py
@@ -0,0 +1,10 @@
+#print a character is vowel or not.
+ch="A";
+a=ch.lower()
+for i in ch:
+    if i=='a' or  i=='e' or  i=='i' or i=='o' or  i=='u' or  i=='A' or  i=='E' or i=='I' or  i=='O' or  i=='U':
+        print("vowel")
+    else:
+        print("Not Vowel")
+
+
