e9db591709e80f0dfe9373b4f8d1c0171a288895
Sampa Nayak <sampanayak987@gmail.com>
2025-08-25 00:51:23 +0530

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,7 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+if(a*a+b*b==c*c):
+    print("Triangle")
+else:
+    print("Not Triangle")
