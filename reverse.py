c293c6a1f9b0c02cc51e758ff3e8e4c613fca0c2
Sampa Nayak <sampanayak987@gmail.com>
2025-08-25 00:14:41 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,21 @@
+#PRINT REVERSE OF A STRING USING USER INPUT.
+'''name=input("enter the string")
+rev_str=''
+for i in range(1,len(name)+1):
+    rev_str=rev_str+name[-i]
+print(rev_str)'''
+
+#PRINT REVERSE OF A STRING.
+name="nayan";
+rev_str=''
+for i in range(1,len(name)+1):
+     rev_str=rev_str+name[-i]
+print(rev_str)
+
+#PRINT REVERSE A INTEGER NUMBER.
+'''n=int(input("enter the number:"))
+rev=0
+while n!=0:
+    rev=rev*10+n%10
+    n=n//10
+print(rev)'''
