71b50b0032118cd278c2eb863634763ef2550915
Sampa Nayak <sampanayak987@gmail.com>
2025-08-25 00:18:34 +0530

    Add files via upload
    count length of string.
    Take a user input of a string and count no of vowels present in the string and print another string without those vowels.

---
+++ string_count.py
@@ -0,0 +1,19 @@
+#count length of string.
+name=input("enter the string:")
+count=0
+for i in name:
+    count+=1
+print(count)
+
+#Take a user input of a string and count no of vowels present in the string and print another string without those vowels.
+'''str=input("enter the string:")
+count=0
+print("Consonant : ")
+for i in str:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        count+=1
+    else:
+        print(i)
+print("No of vowel : ",count)'''
+
+ 
