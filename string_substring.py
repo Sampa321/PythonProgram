0779aacef7fd8fd9170a36ef715c39d022f16fd0
Sampa Nayak <sampanayak987@gmail.com>
2025-08-25 00:23:18 +0530

    Add files via upload
    Check a substring present in a string or not.

---
+++ string_substring.py
@@ -0,0 +1,17 @@
+#check a sub string present in a string or not.
+'''l=[]
+count=0
+str=input("enter the string:")
+l=str.split()
+sub_str=input("enter substring:")
+for i in l:
+    if sub_str==i:
+        count=1
+if count==1:
+    print("yes present")
+else:
+    print("Not Present")'''
+
+
+ 
+
