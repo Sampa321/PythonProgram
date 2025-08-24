b1dbf5bc93fc24a962994cc41fb1252cd036c838
Sampa Nayak <sampanayak987@gmail.com>
2025-08-24 23:15:56 +0530

    Add files via upload

---
+++ open_read_write_append.py
@@ -0,0 +1,29 @@
+#how to create a file:
+'''f=open('python.txt','x')
+print("file create sucessfully")
+f.close()'''
+
+#how to write a file:
+'''f=open('python.txt','w')
+f.write("svu python class")
+print("write sucessfully")'''
+
+#how to read a file:
+'''f=open('python.txt','r')
+print(f.read())'''
+
+#how to append a file:
+'''f=open('python.txt','a')
+print("\n")
+f.write(" svu python class")
+print("write sucessfully")'''
+
+#Question-10(Make a file named as"demo.txt" in python ,write your introduction in that particular file and later read the file in python as well by using(file handing))
+'''f=open('demo.txt','x')
+print("File create sucessfully")
+f.close()
+f=open('demo.txt','w')
+f.write("svu python exam")
+print("file write sucessfully")
+f=open('demo.txt','r')
+f.read(())'''
